3386b16c8bd0f9c1a49b80ce7b96cb9146b3d573
jeremyperwira <jeremyperwira@protonmail.com>
2024-07-15 21:48:32 +0700

    fonts

--- src/components/Preview/ATS/FontSetting.tsx
+++ src/components/Preview/ATS/FontSetting.tsx
@@ -36,6 +36,8 @@ const FontSettings: React.FC = () => {
             onChange={handleFontChange}
             className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
           >
+            <option value="Poppins">Poppins</option>
+
             <option value="Arial" className="font-sans">
               Arial
             </option>
