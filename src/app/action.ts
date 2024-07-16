667f32935f4ceafd2979712edb779b4c3c195c11
jeremyperwira <jeremyperwira@protonmail.com>
2024-07-16 22:54:03 +0700

    chore: code refactor

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,21 +1,31 @@
-'use server';
+"use server";
 
-import { streamText } from 'ai';
-import { openai } from '@ai-sdk/openai';
-import { createStreamableValue } from 'ai/rsc';
-import { canMakeRequest, updateRequestTime, MAX_REQUESTS_PER_HOUR } from '@/lib/limit';
+import { streamText } from "ai";
+import { openai } from "@ai-sdk/openai";
+import { createStreamableValue } from "ai/rsc";
+import {
+  canMakeRequest,
+  updateRequestTime,
+  MAX_REQUESTS_PER_HOUR,
+} from "@/lib/limit";
 
 export async function rewriteSummary(summary: string) {
   if (!canMakeRequest()) {
-    throw new Error(`Batas Pemakaian AI kamu sudah Habis! Upgrade ke Paket Senyum untuk Nikmati Kuota Lebih.`);
+    throw new Error(
+      `Batas Pemakaian AI kamu sudah Habis! Upgrade ke Paket Senyum untuk Nikmati Kuota Lebih.`
+    );
   }
 
-  const stream = createStreamableValue('');
+  const stream = createStreamableValue("");
   const { textStream } = await streamText({
-    model: openai('gpt-3.5-turbo'),
+    model: openai("gpt-3.5-turbo"),
     messages: [
-      { role: 'system', content: 'You are a professional CV writer. Rewrite the given summary in a more professional and concise manner.' },
-      { role: 'user', content: summary }
+      {
+        role: "system",
+        content:
+          "You are a professional CV writer. Rewrite the given summary in a more professional and concise manner. The summary should follow ATS (Applicant tracking system) standard, pick perfect language. Match user summary language unless they ask specifically in English (Mainly for Bahasa Indonesian and English)",
+      },
+      { role: "user", content: summary },
     ],
   });
 
@@ -30,16 +40,19 @@ export async function rewriteSummary(summary: string) {
   return { output: stream.value };
 }
 
-
 export async function generateFullCV(cvData: any) {
-  const stream = createStreamableValue('');
+  const stream = createStreamableValue("");
 
   (async () => {
     const { textStream } = await streamText({
-      model: openai('gpt-3.5-turbo'),
+      model: openai("gpt-3.5-turbo"),
       messages: [
-        { role: 'system', content: 'You are a professional CV writer. Generate a full CV based on the provided data.' },
-        { role: 'user', content: JSON.stringify(cvData) }
+        {
+          role: "system",
+          content:
+            "You are a professional CV writer. Generate a full CV based on the provided data.",
+        },
+        { role: "user", content: JSON.stringify(cvData) },
       ],
     });
 
--- src/components/ui/popup.tsx
+++ src/components/ui/popup.tsx
@@ -1,6 +1,8 @@
-'use client'
+"use client";
+import { Button } from "@nextui-org/button";
 import { useEffect, useState } from "react";
 import { IoIosWarning } from "react-icons/io";
+import { FaThumbsUp } from "react-icons/fa";
 
 interface PopupProps {
   isOpen: boolean; // Prop removed since it will be managed internally
@@ -43,29 +45,29 @@ const Popup: React.FC<PopupProps> = ({ isOpen }) => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-      <div className="z-50 bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg max-w-sm mx-auto">
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-lg  flex font-semibold text-gray-800 dark:text-white">
-            <IoIosWarning className="self-center mr-2"/> Dalam Pengujian Beta
+      <div className="z-50 bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg max-w-sm mx-auto ">
+        <div className="flex justify-center items-center mb-4">
+          <h2 className="text-lg font-semibold text-gray-800 dark:text-white flex items-center">
+            <IoIosWarning className="self-center mr-2 text-red-400" /> Dalam
+            Pengujian Beta
           </h2>
-          <button
-            className="text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-white focus:outline-none"
-            onClick={handleClose}
-          >
-            <svg className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-              <path
-                fillRule="evenodd"
-                d="M14.293 5.293a1 1 0 011.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 111.414-1.414L10 8.586l4.293-4.293z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
         </div>
         <p className="text-sm text-gray-700 dark:text-gray-300">
-          Applikasi masih dalam proses pengembangan dan kemungkinan akan terdapat beberapa kendala dalam pembuatan Resume.
-          Data anda tidak tersimpan ataupun terkirim ke server. Pengujian beta ditujukan untuk memperoleh hasil testing yang 
-          nantinya digunakan sebagai evaluasi pengembangan. -JP
+          Aplikasi masih dalam proses pengembangan dan kemungkinan akan terdapat
+          beberapa kendala dalam pembuatan Resume. Data anda tidak tersimpan
+          ataupun terkirim ke server. Pengujian beta ditujukan untuk memperoleh
+          hasil testing yang nantinya digunakan sebagai evaluasi pengembangan.
+          -JP
         </p>
+        <div className="flex justify-center mt-4">
+          <Button
+            className="text-green-500 flex items-center bg-white border-2 border-green-500 hover:bg-green-500 dark:text-gray-400 hover:text-white dark:hover:text-white focus:outline-none"
+            onClick={handleClose}
+          >
+            <FaThumbsUp className="" />
+            Mengerti
+          </Button>
+        </div>
       </div>
     </div>
   );
