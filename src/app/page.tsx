9ab8271bec38ae24e26ba0b0c77d5f8c9e53eecc
jeremyperwira <jeremyperwira@protonmail.com>
2024-07-17 16:49:32 +0700

    rm: old redux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-import { Provider } from "react-redux";
-import { store } from "../redux/store";
+// import { Provider } from "react-redux";
+// import { store } from "../redux/store";
 import CVEditor from "../components/CVEditor";
 import CVPreview from "../components/CVPreview";
 import Popup from "@/components/ui/popup";
 
 export default function Home() {
   return (
-    // <Provider store={store}>
       <main className="flex flex-col md:flex-row min-h-screen bg-gray-100 dark:bg-gray-900 pt-20 ">
         <Popup isOpen={true} />
         <div className="md:w-1/2 overflow-y-auto bg-blue-50/20 dark:bg-blue-900/20 pt-10 ps-7 pe-5  min-fit rounded-r-3xl border-2 m-4 border-blue-400 dark:bg-gray-800 dark:border-gray-600">
@@ -18,6 +17,5 @@ export default function Home() {
           <CVPreview />
         </div>
       </main>
-    // </Provider>
   );
 }
